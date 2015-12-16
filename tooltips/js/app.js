6c79cab03dcb544ad7f561114725adbcffc2412c
Fricco83 <pktypa@gmail.com>
2015-12-16 11:40:40 +0100

    created simple tooltips vanilla js

---
+++ tooltips/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var tooltipList = document.querySelectorAll(".tooltip");
+	for(var i=0; i<tooltipList.length; i++) {
+		tooltipList[i].addEventListener("mouseover", function(event) {
+			var tooltipText = this.dataset.text;
+			var newElement = document.createElement("SPAN");
+			newElement.classList.add("tooltipText");
+			newElement.innerHTML = tooltipText;
+			this.appendChild(newElement);
+		})
+		tooltipList[i].addEventListener("mouseout",function(event) {
+			var elementToRemove = this.querySelector(".tooltipText");
+			this.removeChild(elementToRemove);
+		});
+	}
+});
+
+//<span class="tooltipText">Teskt tooltipa</span>
