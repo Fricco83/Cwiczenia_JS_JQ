052d247c138ef00918b0b84023c9491c81201867
Fricco83 <pktypa@gmail.com>
2015-12-17 16:58:24 +0100

    slider infinite with small bug

--- Day_second/Slider_JQ_Simple_Infinite/js/app.js
+++ Day_second/Slider_JQ_Simple_Infinite/js/app.js
@@ -1,29 +1,35 @@
 $(document).ready(function () {
 	var ul = $(".slider ul");
+	var firstUlChild = ul.children().first();
+	var lastUlChild = ul.children().last();
+	firstUlChild.before(lastUlChild.clone());
+	lastUlChild.after(firstUlChild.clone());
 	var nextButton = $("#nextPicture");
 	var prevButton = $("#prevPicture");
 	var pictures = $(".slider img");
 	var imageWidth = 400;
-	var currentPicturePosition = 0;
+	var currentPicturePosition = 1;
 
 	ul.css("width", imageWidth * pictures.length + "px");
 	ul.css("left", -currentPicturePosition * imageWidth + "px");
 	nextButton.on("click", function(event) {
-		currentPicturePosition++;
-		if(currentPicturePosition >= pictures.length) {
-			currentPicturePosition = pictures.length - 1;
-		} 
+		currentPicturePosition++; 
 		ul.animate({ "left": -currentPicturePosition * imageWidth + "px"
 		}, 1000, function() {
-				
+			if(currentPicturePosition === pictures.length -1) {
+				currentPicturePosition  = 1;
+				ul.css("left", -currentPicturePosition * imageWidth + "px");
+			}
 		});
 	});
 	prevButton.on("click", function(event) {
 		currentPicturePosition--;
-		if(currentPicturePosition < 0) {
-			currentPicturePosition = 0;
-		}
-			ul.animate({ "left": -currentPicturePosition * imageWidth + "px"
-		}, 1000 );
+		ul.animate({ "left": -currentPicturePosition * imageWidth + "px"
+		}, 1000, function() {
+			if(currentPicturePosition === 0) {
+				currentPicturePosition = pictures.length -2;
+				ul.css("left", -currentPicturePosition * imageWidth + "px");
+			}
+		});
 	});
 });
